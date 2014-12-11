bab6a21afcbd74e35e5e1a0275a84575011d095d
Anthony Garcia <agarcia967@hotmail.com>
2014-12-10 20:26:21 -0800

    v3 - Uses Dictionaries and Tuples

--- CoinTracker.py
+++ CoinTracker.py
@@ -1,19 +1,18 @@
 # Coin Tracker.py
 __author__ = "Anthony R. Garcia <agarcia967@hotmail.com>"
-__date__ = "10/28/2014"
-__version__ = "2.0"
+__date__ = "11/07/2014"
+__version__ = "3.0" #now with dictionaries and tuples!
 __credits__ = None
 
 import os
-from coin_parsing import parseBillsLine
-from coin_parsing import parseCoinsLine
 
 FILENAME = "cointracker.ini"
 CENT_SYMBOL = u"\u00A2"
-SECTIONS = ["bills","coins"]
-KEYS = [["hundred","fifty","twenty","ten","five","two","one"],
-        ["dollar","halfdollar","quarter","dime","nickel","penny"]]
-values = [[0 for i in range(len(KEYS[i]))] for i in range(len(KEYS))]
+SECTIONS = ("bills","coins")
+BILLS = ("hundred","fifty","twenty","ten","five","two","one")
+COINS = ("dollar","halfdollar","quarter","dime","nickel","penny")
+billsCoins= {"hundred":0,"fifty":0,"twenty":0,"ten":0,"five":0,"two":0,"one":0,
+             "dollar":0,"halfdollar":0,"quarter":0,"dime":0,"nickel":0,"penny":0}
 changesMade = False
 
 def saveFile():
@@ -23,17 +22,18 @@ def saveFile():
     outFile.write("#DO NOT move this file!\n")
     sectionCounter = 0
     for section in SECTIONS:
-        keyCounter = 0
         outFile.write("[%s]\n" %section.upper())
-        for key in KEYS[sectionCounter]:
-            outFile.write("%s=%s\n" %(KEYS[sectionCounter][keyCounter],values[sectionCounter][keyCounter]))
-            keyCounter+=1
+        for key in billsCoins:
+            if(key in COINS and section=="coins"):
+                outFile.write("%s=%s\n" %(key,str(billsCoins[key])))
+            elif(key in BILLS and section=="bills"):
+                outFile.write("%s=%s\n" %(key,str(billsCoins[key])))
         sectionCounter+=1
     outFile.close()
     print("Done.")
     changesMade = False
 
-def readINIFile():
+def readINIFile(FILENAME="cointracker.ini"):
     print("Reading file '%s'. Please wait... " %FILENAME, end='')
     if(not(os.path.exists(FILENAME))):
         print("File does not exist.")
@@ -63,8 +63,8 @@ def readINIFile():
                 ll[ll.index(item)] = item.strip()
             key = ll[0]
             value = int(ll[1])
-            if(key in KEYS[currentSection]):
-                values[currentSection][KEYS[currentSection].index(key)] = value
+            if(key in billsCoins):
+                billsCoins[key] = value
             else:
                 print("FILE READ ERROR\n::Invalid Key in Section %s: %s" %(SECTIONS[currentSection],key))
                 return
@@ -75,83 +75,80 @@ def readINIFile():
     print("Done.")
 
 def printValues():
-    print("\n BILLS")
-    print(" $100.00 - %d" %values[0][0])
-    print(" $ 50.00 - %d" %values[0][1])
-    print(" $ 20.00 - %d" %values[0][2])
-    print(" $ 10.00 - %d" %values[0][3])
-    print(" $ 5.00 - %d" %values[0][4])
-    print(" $ 2.00 - %d" %values[0][5])
-    print(" $ 1.00 - %d" %values[0][6])
-    billsTotal = sum([values[0][0]*100,
-                      values[0][1]*50,
-                      values[0][2]*20,
-                      values[0][3]*10,
-                      values[0][4]*5,
-                      values[0][5]*2,
-                      values[0][6]*1])
+    print("\n  BILLS")
+    print(" $100.00 - %d" %billsCoins['hundred'])
+    print(" $ 50.00 - %d" %billsCoins['fifty'])
+    print(" $ 20.00 - %d" %billsCoins['twenty'])
+    print(" $ 10.00 - %d" %billsCoins['ten'])
+    print(" $  5.00 - %d" %billsCoins['five'])
+    print(" $  2.00 - %d" %billsCoins['two'])
+    print(" $  1.00 - %d" %billsCoins['one'])
+    billsTotal = sum([billsCoins['hundred']*100,
+                      billsCoins['twenty']*50,
+                      billsCoins['one']*20,
+                      billsCoins['ten']*10,
+                      billsCoins['five']*5,
+                      billsCoins['two']*2,
+                      billsCoins['one']*1])
     print("Bills Total: $%0.2f" %billsTotal)
-    print("\n COINS")
-    print(" $1.00 - %3.0d" %values[1][0])
-    print(" 50%s - %3.0d" %(CENT_SYMBOL,values[1][1]))
-    qR = values[1][2]//40
-    dR = values[1][3]//50
-    nR = values[1][4]//40
-    pR = values[1][5]//50
+    print("\n  COINS")
+    print(" $1.00 - %3.0d" %billsCoins['dollar'])
+    print("   50%s - %3.0d" %(CENT_SYMBOL,billsCoins['halfdollar']))
+    q = billsCoins['quarter']
+    d = billsCoins['dime']
+    n = billsCoins['nickel']
+    p = billsCoins['penny']
+    qR, dR, nR, pR = q//40, d//50, n//40, p//50
     e=""
     if(qR):
         e="(%s)" %qR
-        print(" 25%s - %3.0d %s" %(CENT_SYMBOL,values[1][2],e))
+    print("   25%s - %3.0d %s" %(CENT_SYMBOL,q,e))
+    
     e=""
     if(dR):
         e="(%s)" %dR
-        print(" 10%s - %3.0d %s" %(CENT_SYMBOL,values[1][3],e))
+    print("   10%s - %3.0d %s" %(CENT_SYMBOL,d,e))
     e=""
     if(nR):
         e="(%s)" %nR
-        print(" 5%s - %3.0d %s" %(CENT_SYMBOL,values[1][4],e))
+    print("    5%s - %3.0d %s" %(CENT_SYMBOL,n,e))
     e=""
     if(pR):
         e="(%s)" %pR
-    print(" 1%s - %3.0d %s" %(CENT_SYMBOL,values[1][5],e))
-    coinsTotal = sum([values[1][0]*1,
-                      values[1][1]*.5,
-                      values[1][2]*.25,
-                      values[1][3]*.10,
-                      values[1][4]*.05,
-                      values[1][5]*.01,])
+    print("    1%s - %3.0d %s" %(CENT_SYMBOL,p,e))
+    coinsTotal = sum([billsCoins['dollar']*1,
+                      billsCoins['halfdollar']*.5,
+                      q*.25, d*.10, n*.05, p*.01,])
     print("Coins Total: $%0.2f\n" %coinsTotal)
-    print("Total: $%0.2f\n" %currentValue())
 
 def calcDeposit():
     print("\nCalculating...")
-    depositAll = 0
+    depositAll = False
     depositValue = 0
-    depositValue+=values[0][0]*100
-    depositValue+=values[0][1] *50
-    depositValue+=values[0][2] *20
-    depositValue+=values[0][3] *10
-    depositValue+=values[0][4] * 5
+    depositValue+=billsCoins['hundred']*100
+    depositValue+=billsCoins['fifty'] *50
+    depositValue+=billsCoins['twenty'] *20
+    depositValue+=billsCoins['ten'] *10
+    depositValue+=billsCoins['five'] * 5
     if(depositAll):
-        depositValue+=values[0][5] * 2
+        depositValue+=billsCoins['two'] * 2
     else:
         print("[$2 bills omitted]")
-    depositValue+=values[0][6] * 1
+    depositValue+=billsCoins['one'] * 1
     billsOnly = depositValue
-    
+
     if(depositAll):
-        depositValue+=values[1][0] *1.00
+        depositValue+=billsCoins['dollar'] *1.00
     else:
         print("[$1 coins omitted]")
-    
     if(depositAll):
-        depositValue+=values[1][1] * .50
+        depositValue+=billsCoins['halfdollar'] * .50
     else:
         print("[50%s coins omitted]" %CENT_SYMBOL)
-    depositValue+=((values[1][2]//40)*10)
-    depositValue+=((values[1][3]//50)*5)
-    depositValue+=((values[1][4]//40)*2)
-    depositValue+=((values[1][5]//50)*.5)
+    depositValue+=((billsCoins['quarter']//40)*10)
+    depositValue+=((billsCoins['dime']//50)*5)
+    depositValue+=((billsCoins['nickel']//40)*2)
+    depositValue+=((billsCoins['penny']//50)*.5)
     print("Done.")
     print("\nBills Only: $%0.2f" %billsOnly)
     print("Deposit value: $%0.2f\n" %depositValue)
@@ -164,54 +161,55 @@ def makeDeposit():
     if(not(resp[0].lower()=="y")):
         return
     depositAll = 0
-    values[0][0] = 0 #hundreds
-    values[0][1] = 0 #fifties
-    values[0][2] = 0 #twenties
-    values[0][3] = 0 #tens
-    values[0][4] = 0 #fives
+    billsCoins['hundred'] = 0 #hundreds
+    billsCoins['fifty'] = 0 #fifties
+    billsCoins['twenty'] = 0 #twenties
+    billsCoins['ten'] = 0 #tens
+    billsCoins['five'] = 0 #fives
     if(depositAll):
-        values[0][5] = 0 #twos
+        billsCoins['two'] = 0 #twos
     else:
         print("[$2 bills omitted]")
-    values[0][6] = 0 #ones
-    
+    billsCoins['one'] = 0 #ones
+
     if(depositAll):
-        values[1][0] = 0 #dollar coins
+        billsCoins['dollar'] = 0 #dollar coins
     else:
         print("[$1 coins omitted]")
     if(depositAll):
-        values[1][1] = 0 #fifty cent coins
+        billsCoins['halfdollar'] = 0 #fifty cent coins
     else:
         print("[50%s coins omitted]" %CENT_SYMBOL)
-    values[1][2] = values[1][2] % 40 #quarters
-    values[1][3] = values[1][3] % 50 #dimes
-    values[1][4] = values[1][4] % 40 #nickels
-    values[1][5] = values[1][5] % 50 #pennies
+    billsCoins['quarter'] = billsCoins['quarter'] % 40 #quarters
+    billsCoins['dime'] = billsCoins['dime'] % 50 #dimes
+    billsCoins['nickel'] = billsCoins['nickel'] % 40 #nickels
+    billsCoins['penny'] = billsCoins['penny'] % 50 #pennies
     print("Deposited amounts removed from current values.")
     changesMade = True
 
 def currentValue():
     totalValue = 0
-    totalValue+=values[0][0]*100
-    totalValue+=values[0][1] *50
-    totalValue+=values[0][2] *20
-    totalValue+=values[0][3] *10
-    totalValue+=values[0][4] * 5
-    totalValue+=values[0][5] * 2
-    totalValue+=values[0][6] * 1
-    totalValue+=values[1][0] *1.00
-    totalValue+=values[1][1] * .50
-    totalValue+=values[1][2] * .25
-    totalValue+=values[1][3] * .10
-    totalValue+=values[1][4] * .05
-    totalValue+=values[1][5] * .01
+    totalValue+=billsCoins['hundred']*100
+    totalValue+=billsCoins['fifty'] *50
+    totalValue+=billsCoins['twenty'] *20
+    totalValue+=billsCoins['ten'] *10
+    totalValue+=billsCoins['five'] * 5
+    totalValue+=billsCoins['two'] * 2
+    totalValue+=billsCoins['one'] * 1
+
+    totalValue+=billsCoins['dollar'] *1.00
+    totalValue+=billsCoins['halfdollar'] * .50
+    totalValue+=billsCoins['quarter'] * .25
+    totalValue+=billsCoins['dime'] * .10
+    totalValue+=billsCoins['nickel'] * .05
+    totalValue+=billsCoins['penny'] * .01
     return totalValue
 
 def coinRollers(isPrinter):
-    qRolls = values[1][2]//40
-    dRolls = values[1][3]//50
-    nRolls = values[1][4]//40
-    pRolls = values[1][5]//50
+    qRolls = billsCoins['quarter']//40
+    dRolls = billsCoins['dime']//50
+    nRolls = billsCoins['nickel']//40
+    pRolls = billsCoins['penny']//50
     if(isPrinter):
         if(sum([qRolls,dRolls,nRolls,pRolls])<1):
             print("No coin rollers.")
@@ -249,32 +247,74 @@ def addMoney():
     print("\nAdd Bills & Coins\n-----------------\nType 'c' to cancel")
     counter = 0
     print("\nBILLS")
+    for item in KEYS[0]:
+        valid = 0
+        while(not(valid)):
+            try:
+                i = input("%s: "%item)
+                if(i==""):
+                    ValueError
+                elif(i[0].lower()=="c"):
+                    return
+                #i = int(i)
+                values[0][counter] += int(i)
+                changesMade = True
+                valid = 1
+            except ValueError:
+                print("Integers only!")
+                valid = 0
+        counter+=1
+    counter = 0
+    print("\nCOINS")
+    for item in KEYS[1]:
+        valid = 0
+        while(not(valid)):
+            try:
+                i = input("%s: "%item)
+                if(i[0].lower()=="c"):
+                    return
+                #i = int(i)
+                values[1][counter] += int(i)
+                changesMade = True
+                valid = 1
+            except ValueError:
+                print("Integers only!")
+                valid = 0
+        counter+=1
     billsList = parseBillsLine()
     if(billsList!=None):
-        z = 0
         changesMade = True
-        while(z<len(values[0])):
-            values[0][z]+=billsList[z]
-            z+=1
+        billsCoins['hundred']+=billsList[0]
+        billsCoins['fifty']  +=billsList[1]
+        billsCoins['twenty'] +=billsList[2]
+        billsCoins['ten']    +=billsList[3]
+        billsCoins['five']   +=billsList[4]
+        billsCoins['two']    +=billsList[5]
+        billsCoins['one']    +=billsList[6]
     else:
         return
     print("\nCOINS")
     coinsList = parseCoinsLine()
     if(coinsList!=None):
         changesMade = True
-        z = 0
-        while(z<len(values[1])):
-            values[1][z]+=coinsList[z]
-            z+=1
+        billsCoins['dollar']    +=coinsList[0]
+        billsCoins['halfdollar']+=coinsList[1]
+        billsCoins['quarter']   +=coinsList[2]
+        billsCoins['dime']      +=coinsList[3]
+        billsCoins['nickel']    +=coinsList[4]
+        billsCoins['penny']     +=coinsList[5]
     else:
         while(True):
             resp = input("Clear bills? (y/n) ")
             if(len(resp)>0 and resp[0].lower()=="y"):
                 changesMade = False
-                z = 0
-                while(z<len(values[0])):
-                    values[0][z]-=billsList[z]
-                    z+=1
+                billsCoins['hundred']-=billsList[0]
+                billsCoins['fifty']  -=billsList[1]
+                billsCoins['twenty'] -=billsList[2]
+                billsCoins['ten']    -=billsList[3]
+                billsCoins['five']   -=billsList[4]
+                billsCoins['two']    -=billsList[5]
+                billsCoins['one']    -=billsList[6]
                 print("Bills cleared.")
                 return
             elif(len(resp)>0 and resp[0].lower()=="n"):
@@ -292,7 +332,6 @@ menu = ["Add Bills & Coins",
         "Make Deposit",
         "Reload file",
         "Save & Quit"]
-
 def menuMaker():
     counter = 1
     for item in menu:
@@ -316,7 +355,7 @@ def main():
                 if(not(changesMade)):
                     return
                 while(1==1):
-                    resp = input("Save changes? (y/n) ")
+                    resp = input("Save changes? (y/n)")
                     if(("yes" in resp.lower()) or ("y" in resp.lower())):
                         saveFile()
                         return
@@ -338,13 +377,13 @@ def main():
             elif(resp==5): #menu[5] "Make Deposit"
                 makeDeposit()
             elif(resp==6): #menu[6] "Reload file"
-                readINIFile()
+                readINIFile('myfile.txt')
             elif(resp==7): #menu[7] "Save & Quit"
                 saveFile()
                 return
             else:
                 print("Please enter a number in range 0-%s" %len(menu))
-                input("Press ENTER to continue.")
+            input("Press ENTER to continue.")
         except ValueError:
             print("Please enter numbers only.")
             input("Press ENTER to continue.")
